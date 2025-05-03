bdf4338dcac2c87c3276e9f038f1626b0241a752
AhmedAfzal2 <ahmedafzal6705@gmail.com>
2025-05-03 18:49:35 +0500

    .

---
+++ FileSystem.py
@@ -0,0 +1,251 @@
+from settings import BLOCK_SIZE, FREE_START, TOTAL_MEMORY
+from nodes import Directory, File
+from bitarray import bitarray
+import pickle
+import os
+
+class FileSystem:
+    def __init__(self, file_name):
+        if os.path.exists(file_name):
+            self.file = open(file_name, 'r+b')
+            self.load()
+        else:
+            self.file = open(file_name, 'w+b')
+            self.root: Directory = Directory('/')
+            # denotes free blocks, custom bitarray() is used as it is much smaller to store in file
+            self.free_spaces = bitarray((TOTAL_MEMORY - FREE_START) // BLOCK_SIZE)
+            self.free_spaces.setall(True)
+            self.save()
+
+        self.current_path: list[Directory] = [self.root]
+        self.opened_files = []
+        
+    def save(self):
+        # as root contains references to all its children which further contain references, simply pickling the root stores the entire tree
+        metadata = {
+            'free': self.free_spaces,
+            'root': self.root
+        }
+        
+        data = pickle.dumps(metadata)
+        
+        if len(data) > FREE_START:
+            raise MemoryError("Directory tree is too big to store in provided space, consider increasing FREE_START in settings.")
+        
+        # pad nulls for cleanliness
+        data += b'\x00' * (FREE_START - len(data))
+        
+        self.file.seek(0)
+        self.file.write(data)
+        
+    def load(self):
+        self.file.seek(0)
+        data = self.file.read(FREE_START)
+        metadata = pickle.loads(data.rstrip(b'\x00'))   # rstrip to remove null padding
+        self.free_spaces = metadata['free']
+        self.root = metadata['root']
+        self.set_fs(self.root)
+        
+    def set_fs(self, root):
+        for child in root.children:
+            if type(child) == File:
+                child.fs = self
+            else:
+                self.set_fs(child)
+                
+    def is_abs(self, path: str):
+        if path.startswith('/'):
+            return True
+        return False
+                
+    def str_to_path(self, path: str):
+        if self.is_abs(path):
+            node = self.root
+            path_list = path.split('/')[1:]     # first is just ''
+        else:   # relative path
+            node = self.current_path[-1]
+            path_list = path.split('/')
+            
+        while '' in path_list:
+            path_list.remove('')
+            
+        return node, path_list
+    
+    def search_dir(self, dir: Directory, name: str, t):
+        for node in dir.children:
+            if node.name == name and type(node) == t:
+                return node
+        return None
+    
+    def search_path(self, path: str, t, parent=False, warn=False):
+        node, path_list = self.str_to_path(path)
+        prev_node = node
+        for i in range(len(path_list) - 1):
+            prev_node = node
+            node = self.search_dir(node, path_list[i], Directory)
+            if node == None:
+                if warn:
+                    print(f"Directory {'/' + '/'.join(path_list[:i+1])} does not exist.")
+                return None
+                
+        node = self.search_dir(node, path_list[-1], t)
+        if node == None and warn:
+            print(f"{path} does not exist.")
+        
+        if not parent:
+            return node
+        else:
+            return node, prev_node
+        
+    def mkdir(self, path: str, file=False):
+        node, path_list = self.str_to_path(path)
+        
+        # only need to check if first one exists
+        if self.search_dir(node, path_list[0], Directory):
+            print(f"Directory {'/' * self.is_abs(path) + path_list[0]} already exists.")
+            return
+        
+        if file and len(path_list) == 1 and self.search_dir(node, path_list[0], File):
+            print(f"File {'/' * self.is_abs(path) + path_list[0]} already exists.")
+            return
+
+        # create each directory
+        for dir_name in path_list[:-1]:
+            this_dir = Directory(dir_name)
+            node.children.append(this_dir)
+            node = this_dir
+            
+        if file:
+            node.children.append(File(path_list[-1], self))
+        else:
+            node.children.append(Directory(path_list[-1]))
+
+        self.save()
+        
+        return node.children[-1]
+
+    def chdir(self, path: str):
+        # to go backwards
+        if path == ".." and len(self.current_path) > 1:
+            self.current_path.pop()
+            return
+        
+        node, path_list = self.str_to_path(path)
+        if self.is_abs(path):
+            self.current_path = [self.root]
+        
+        for i in range(len(path_list)):
+            found = self.search_dir(self.current_path[-1], path_list[i], Directory)
+            if found:
+                self.current_path.append(found)
+            else:
+                print(f"Directory {'/' * self.is_abs(path) + '/'.join(path_list[:i+1])} does not exist.")
+            
+    def ls(self):
+        if len(self.current_path[-1].children) == 0:
+            print("Empty")
+        else:
+            for child in self.current_path[-1].children:
+                print(child.name)
+
+    def create(self, path: str) -> File:
+        return self.mkdir(path, True)
+
+    def delete_file(self, path: str):
+        found, parent = self.search_path(path, File, True, True)
+        if found:
+            found.truncate_file(0)
+            parent.children.remove(found)
+            del found
+            self.save()
+
+    def delete_file_t(self, file: File, parent: Directory):
+        file.truncate_file(0)
+        parent.children.remove(file)
+        del file
+        self.save()
+        
+    def delete_dir_t(self, dir: Directory, parent: Directory):
+        for child in dir.children:
+            if type(child) == File:
+                self.delete_file_t(child, dir)
+            else:
+                self.delete_dir_t(child, dir) 
+        parent.children.remove(dir)
+        del dir
+        self.save()
+
+    def delete_dir(self, name: str):
+        dir = self.search_path(name, Directory)
+        if dir:
+            for child in dir.children:
+                if type(child) == File:
+                    self.delete_file_t(child, self.current_path[-1])
+                else:
+                    self.delete_dir_t(child, dir)
+            self.current_path[-1].children.remove(dir)
+            del dir
+            self.save()
+            
+    def move_file(self, src: str, dest: str):
+        found_src, parent_src = self.search_path(src, File, True, True)
+        found_dest = self.search_path(dest, Directory, False, True)
+        if not found_src or not found_dest:
+            return
+        
+        found_dest.children.append(found_src)
+        parent_src.children.remove(found_src)
+        
+    def move_dir(self, src: str, dest: str):
+        found_src, parent_src = self.search_path(src, Directory, True, True)
+        found_dest = self.search_path(dest, Directory, False, True)
+        if not found_src or not found_dest:
+            return
+        
+        found_dest.children.append(found_src)
+        parent_src.children.remove(found_src)
+            
+    # returns start index of a free block
+    def allocate(self) -> int:
+        for i, space in enumerate(self.free_spaces):
+            if space:
+                self.free_spaces[i] = False
+                return (i + FREE_START // BLOCK_SIZE) * BLOCK_SIZE
+
+    def open(self, name: str, mode: str) -> File:
+        #REFEX HERE
+        found = self.search_path(name, File)
+
+        # create empty file in write mode
+        if mode == 'w':
+            if found:
+                self.delete_file_t(found, self.current_path[-1])
+            found = self.create(name)
+            
+        if type(found) != File:
+            print(f"File {name} does not exist.")
+            return
+        
+        found.set_mode(mode)
+        self.opened_files.append(found)
+        return found
+    
+    def close(self, file: File):
+        self.opened_files.remove(file)
+        
+    def print_current_path(self):
+        print("PS /", end='')
+        for dir in self.current_path[1:-1]:
+            print(dir.name, end='/')
+        if len(self.current_path) > 1:
+            print(f"{self.current_path[-1].name}", end='')
+        print("> ", end='')
+        
+    def show_memory_map(self):
+        for file in self.opened_files:
+            file.display_details()
+            print('')
+            
+        
+    def __del__(self):
+        self.file.close()
---
+++ parser.py
@@ -0,0 +1,135 @@
+from FileSystem import FileSystem
+from nodes import File
+
+def extract_cmd(str: str):
+    i = str.find(' ')
+    if i == -1:
+        return str.strip().lower()
+    return str[:i].lower()
+
+def extract_args(str: str):
+    i = str.find(' ')
+    if i == -1:
+        return []
+    
+    str = str[i+1:]   # everything except cmd
+    str = str.split(',')
+    for i in range(len(str)):
+        str[i] = str[i].strip()
+    return str
+
+def warn_args(cmd, takes, given):
+    if given != takes:
+        print(f"{cmd} takes exactly {takes} argument(s). {given} were provided.")
+        return True
+    return False
+
+fs = FileSystem("sample.dat")
+
+p = ''
+opened_files: dict[str, File] = {}
+try:
+    while (p != "exit"):
+        fs.print_current_path()
+        p = input()
+        cmd = extract_cmd(p)
+        args = extract_args(p)
+        l = len(args)
+        match cmd:
+            case "create":
+                if warn_args("create", 1, l):
+                    continue
+                fs.create(args[0])
+
+            case "delete_file":
+                if warn_args("delete_file", 1, l):
+                    continue
+                fs.delete_file(args[0])
+
+            case "delete_dir":
+                if warn_args("delete_dir", 1, l):
+                    continue
+                fs.delete_dir(args[0])
+
+            case "mkdir":
+                if warn_args("mkdir", 1, l):
+                    continue
+                fs.mkdir(args[0])
+
+            case "chdir":
+                if warn_args("chdir", 1, l):
+                    continue
+                fs.chdir(args[0])
+
+            case "move_file":
+                if warn_args("move", 2, l):
+                    continue
+                fs.move_file(args[0], args[1])
+                
+            case "move_dir":
+                if warn_args("move", 2, l):
+                    continue
+                fs.move_dir(args[0], args[1])
+            
+            case "open":
+                if warn_args("open", 2, l):
+                    continue
+                opened_files[args[0]] = fs.open(args[0], args[1])
+
+            case "close":
+                if warn_args("close", 1, l):
+                    continue
+                fs.close(opened_files[args[0]])
+                del opened_files[args[0]]
+
+            case "write_to_file":
+                if l != 2 and l != 3:
+                    print(f"write_to_file takes 2 or 3 arguments. {l} were provided.")
+                    continue
+                if args[0] not in opened_files:
+                    print(f"{args[0]} is not opened. Cannot write.")
+                    continue
+                
+                if l == 2:
+                    opened_files[args[0]].write_to_file(fs, args[1].strip('"').strip("'"))
+                else:
+                    opened_files[args[0]].write_to_file(fs, args[1].strip('"').strip("'"), int(args[2]))
+
+            case "read_from_file":
+                if l != 1 and l != 3:
+                    print(f"read_from_file takes 1 or 3 arguments. {l} were provided.")
+                    continue
+                if args[0] not in opened_files:
+                    print(f"{args[0]} is not opened. Cannot read.")
+                    continue
+                
+                if l == 1:
+                    print(opened_files[args[0]].read_from_file(fs))
+                else:
+                    print(opened_files[args[0]].read_from_file(fs, int(args[1]), int(args[2])))
+
+            case "truncate_file":
+                if warn_args("truncate_file", 2, l):
+                    continue
+                if args[0] not in opened_files:
+                    print(f"{args[0]} is not opened. Cannot truncate.")
+                    continue
+                opened_files[args[0]].truncate_file(int(args[1]), fs)
+
+            case "ls":
+                if warn_args("ls", 0, l):
+                    continue
+                fs.ls()
+                
+            case "show_memory_map":
+                if warn_args("show_memory_map", 0, l):
+                    continue
+                fs.show_memory_map()
+                
+            case 'exit':
+                pass
+                
+            case _:
+                print(f"Function {cmd} is not recognized.")
+finally:
+    del fs  # to close the file
